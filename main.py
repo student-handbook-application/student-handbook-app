e4c97940a4960b75458f7a759dbdab658c7d9e7b
qanhYE <quocanh21122003@gmail.com>
2024-03-13 22:34:14 +0700

    Add fast-api

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+import uvicorn
+from  fastapi import FastAPI
+from app.modules import modules_router
+
+app = FastAPI()
+app.include_router(modules_router)
+
+@app.get("/")
+async def index():
+    return {"message": "Simple Question API Services"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=7860)
