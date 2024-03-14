cf20eafae863a8d2f475d9b8caf324ea9b516c5e
Khanhduong123 <khanhbaoduong789@gmail.com>
2024-03-14 23:47:26 +0700

    Phase2/KhanhDB: add chatbot folder

---
+++ chatbot/chatbot_api.py
@@ -0,0 +1,35 @@
+import functools
+
+from flask import (
+    Blueprint, render_template, request, json, jsonify
+)
+from .model import Chatbot
+
+bp = Blueprint('chatbot', __name__)
+
+@bp.route("/get", methods=['POST'])  # Đảm bảo route chỉ chấp nhận phương thức POST
+def get_bot_response():
+    try:
+        # Lấy dữ liệu JSON từ yêu cầu POST
+        data = request.json
+
+        print(data)
+        # Kiểm tra xem dữ liệu có tồn tại và có chứa key 'messages' không
+        if data  and 'messages' in data:
+            print("da chay")
+            messages = data['messages']
+            print(messages)
+            # Trả về nội dung của tin nhắn đầu tiên
+            print(messages[0]['content'])
+            first_message_content = messages[0]['content'] # vd ng dung nhap hello thi no se lay chu hello \
+            print(first_message_content)
+            processed_data = {"processed_message": Chatbot(first_message_content)}
+            
+            # Trả về kết quả xử lý dưới dạng JSON
+            return jsonify(processed_data)
+        else:
+            # Trả về thông báo lỗi nếu dữ liệu không hợp lệ
+            return jsonify({"error": "Invalid request format"}), 400
+    except Exception as e:
+        # Trả về thông báo lỗi nếu có bất kỳ lỗi nào xảy ra trong quá trình xử lý
+        return jsonify({"error": str(e)}), 500
---
+++ chatbot/model.py
@@ -0,0 +1,72 @@
+from qdrant_client import qdrant_client
+import os
+import torch
+from chatbot.auguments import load_auguments
+from sentence_transformers import SentenceTransformer
+from langchain_community.vectorstores import Qdrant
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+from chatbot.chatbot import Model, create_prompt, create_qa_chain
+
+# client = qdrant_client.QdrantClient(
+#     url = os.getenv("QDRANT_HOST"),
+#     api_key = os.getenv("QDRANT_API_KEY")
+# )
+
+# model = SentenceTransformer('keepitreal/vietnamese-sbert')
+
+def Chatbot(msg):
+    torch.cuda.empty_cache()
+    hf_api ,hf_embedding_model, model_id, url_database, api_key_database = load_auguments()
+
+    template = """<|im_start|>system\nSử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra câu trả lời\n
+{context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
+            
+
+
+    client = qdrant_client.QdrantClient(
+    url = url_database,
+    api_key= api_key_database
+    )
+    embeddings = HuggingFaceEmbeddings(model_name = hf_embedding_model)
+
+    doc_store = Qdrant(
+        client=client, collection_name="Pengi-Doc", 
+        embeddings=embeddings,
+    )
+
+    model_embedings = SentenceTransformer(hf_embedding_model)
+    query_vector = model_embedings.encode(msg).tolist()
+
+    hits = client.search(
+    collection_name = "Pengi-FAQ",
+    query_vector = query_vector,
+    limit = 1,
+    )
+
+    # print('da chay')
+    for hit in hits:
+        if hit.score > 0.7:
+            print('da chay 1')
+            return f"Pengi \n{hit.payload['Answers']}"
+        else: 
+            print('da chay 2')
+            model = Model(model_id, hf_api, 0.01)
+            llm = model.load_model()
+            prompt = create_prompt(template)
+    
+            qa_chain = create_qa_chain(llm, doc_store, prompt)
+            return qa_chain.invoke(msg)
+
+    
+    
+    """ Đây là file để đưa model vào"""
+
+""""
+can phai biet la cho nao lay cai prompt ma ng dung nhap vo ()
+
+embedding cai cau get do (function)
+so sanh su tuong dong (fuction)
+POST laij len cau tra loi sau khi so sanh su tuong dong (fuction)
+"""
+
