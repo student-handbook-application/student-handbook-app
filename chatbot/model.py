45ce47fb13632204997721b5e24a519fc9a5b261
Khanhduong123 <khanhbaoduong789@gmail.com>
2024-03-23 15:18:01 +0700

    Phase2/KhanhDB: Update model.py file

--- chatbot/model.py
+++ chatbot/model.py
@@ -8,29 +8,42 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
 from chatbot.chatbot import Model, create_prompt, create_qa_chain
 
+llm = None
+
+def load_llm(doc_store,msg):
+    global llm
+
+    hf_api ,_, model_id, _, _ = load_auguments()
+    template = """<|im_start|>system\nChỉ sử dụng thông tin sau đây để trả lời câu hỏi. Nếu câu hỏi không liên quan dến nội dung sau đây, hãy trả lời rằng bạn không biết câu trả lời, đừng cố sinh thêm thông tin để trả lời\n
+ {context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
+    
+    if llm == None:
+        model = Model(model_id, hf_api, 0.01)
+        llm = model.load_model()
+
+    prompt = create_prompt(template)
+    qa_chain = create_qa_chain(llm, doc_store, prompt)
+
+    return qa_chain.invoke(msg)
+
 
 def Chatbot(msg):
-    torch.cuda.empty_cache()
-    hf_api ,hf_embedding_model, model_id, url_database, api_key_database = load_auguments()
-
-    template = """<|im_start|>system\nChỉ sử dụng thông tin sau đây để trả lời câu hỏi. Nếu câu hỏi không liên quan dến nội dung sau đây, hãy trả lời rằng bạn không biết câu trả lời, đừng cố sinh thêm thông tin để trả lời\n
-{context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
-    model = Model(model_id, hf_api, 0.01)
-    llm = model.load_model()
-    prompt = create_prompt(template) 
-
+    _,hf_embedding_model, _, url_database, api_key_database = load_auguments()
 
     client = qdrant_client.QdrantClient(
     url = url_database,
     api_key= api_key_database
     )
-    embeddings = HuggingFaceEmbeddings(model_name = hf_embedding_model)
 
+    #load database documents
+    embeddings = HuggingFaceEmbeddings(model_name = hf_embedding_model)
     doc_store = Qdrant(
         client=client, collection_name="Pengi-Doc", 
         embeddings=embeddings,
     )
 
+
+    #load database FAQ
     model_embedings = SentenceTransformer(hf_embedding_model)
     query_vector = model_embedings.encode(msg).tolist()
 
@@ -40,10 +53,10 @@ def Chatbot(msg):
     limit = 1,
     )
 
-    # print('da chay')
+
+    #run result
     for hit in hits:
         if hit.score > 0.49:
             return f"{hit.payload['Answers']}"
         else: 
-            qa_chain = create_qa_chain(llm, doc_store, prompt)
-            return qa_chain.invoke(msg)
+            return load_llm(doc_store, msg)
