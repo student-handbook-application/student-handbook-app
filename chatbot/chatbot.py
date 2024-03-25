965f309ef75f3333635dce09cb0bfe3fc47120ee
Khanhduong123 <khanhbaoduong789@gmail.com>
2024-03-25 14:23:17 +0700

    Phase2/KhanhDB: Update chatbot file in folder

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,73 +1,56 @@
+from qdrant_client import qdrant_client
+import os
 import torch
-import langchain
-import langchain_core
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from transformers import pipeline
-from langchain.memory import ConversationBufferMemory
-from langchain_core.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
+from chatbot.auguments import load_auguments
+from sentence_transformers import SentenceTransformer
+from langchain_community.vectorstores import Qdrant
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+from chatbot.model import Model, create_prompt, create_qa_chain
 
-class Model:
-    def __init__(self, model_id: str, token_id: str, temperatures: float) -> None:
-        """
-        Auguments
-        model_id: the folder path of model that you choose in Hugging Face
-        token_id: the token api of hugging face account
-        temperatures: this auguments make model will creative when it is generating
-        """
-
-        self.model_id = model_id
-        self.token_id = token_id
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_id, token= self.token_id)
-        self.temperature = temperatures
-
-        
-
-    def load_model(self):
-        model = AutoModelForCausalLM.from_pretrained(self.model_id,
-                                             device_map='cuda:0',
-                                             torch_dtype=torch.bfloat16,
-                                             use_auth_token=self.token_id,
-                                            #  load_in_4bit=True,
-                                             load_in_8bit=True,
-                        
-        )
-
-        pipe = pipeline("text-generation",
-                        model= model,
-                        tokenizer= self.tokenizer,
-                        torch_dtype= torch.bfloat16,
-                        device_map= "auto",
-                        max_new_tokens = 512,
-                        do_sample = True,
-                        top_k = 15,
-                        num_return_sequences=1,
-                        eos_token_id=self.tokenizer.eos_token_id)
-        
-        llm = HuggingFacePipeline(pipeline=pipe, model_kwargs={"temperature": self.temperature
-                                                               })
-        return llm
+def load_llm():
     
+    hf_api ,_, model_id, _, _ = load_auguments()
 
-def create_prompt(templates: None) -> langchain_core.prompts.prompt.PromptTemplate:
-    qa_chain_prompt = PromptTemplate.from_template(templates)
-    return qa_chain_prompt
+    model = Model(model_id, hf_api, 0.01)
+    llm = model.load_model()
 
+    return llm
 
-def create_qa_chain(llm: any, database_vector: any, prompt: any) -> langchain.chains.retrieval_qa.base.RetrievalQA:
-    memory = ConversationBufferMemory(
-            memory_key='chat_history', return_messages=True)
-    
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type='stuff',  # Change 'stuff' to a valid chain type
-        retriever=  database_vector.as_retriever(search_kwargs={"k": 3}),
-        return_source_documents = False, #trả về src trả lời
-        # memory = memory,
-        chain_type_kwargs={
-            "verbose": True,
-            "prompt": prompt
-        }
+def Chatbot(msg,llm):
+    _,hf_embedding_model, _, url_database, api_key_database = load_auguments()
+    template = """<|im_start|>system\nChỉ sử dụng thông tin sau đây để trả lời câu hỏi. Nếu câu hỏi không liên quan dến nội dung sau đây, hãy trả lời rằng bạn không biết câu trả lời, đừng cố sinh thêm thông tin để trả lời\n
+{context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
+
+    client = qdrant_client.QdrantClient(
+    url = url_database,
+    api_key= api_key_database
     )
-    return qa_chain
+
+    #load database documents
+    embeddings = HuggingFaceEmbeddings(model_name = hf_embedding_model)
+    doc_store = Qdrant(
+        client=client, collection_name="Pengi-Doc", 
+        embeddings=embeddings,
+    )
+
+
+    #load database FAQ
+    model_embedings = SentenceTransformer(hf_embedding_model)
+    query_vector = model_embedings.encode(msg).tolist()
+
+    hits = client.search(
+    collection_name = "Pengi-FAQ",
+    query_vector = query_vector,
+    limit = 1,
+    )
+
+
+    #run result
+    for hit in hits:
+        if hit.score > 0.49:
+            return f"{hit.payload['Answers']}"
+        else: 
+            prompt = create_prompt(template)
+            qa_chain = create_qa_chain(llm, doc_store, prompt)
+            return qa_chain.invoke(msg)
