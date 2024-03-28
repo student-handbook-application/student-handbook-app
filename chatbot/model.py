9ed88e1bf6b7bc723eeb4f5774ff36fd0cc51eee
Khanhduong123 <khanhbaoduong789@gmail.com>
2024-03-28 10:10:17 +0700

    Phase2/KhanhDB: Update chatbot.py and model.py file

--- chatbot/model.py
+++ chatbot/model.py
@@ -7,7 +7,11 @@ from transformers import pipeline
 from langchain.memory import ConversationBufferMemory
 from langchain_core.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
-
+from chatbot.auguments import load_auguments
+from langchain_community.vectorstores import Qdrant
+from qdrant_client import qdrant_client
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
 class Model:
     def __init__(self, model_id: str, token_id: str, temperatures: float) -> None:
         """
@@ -55,14 +59,28 @@ def create_prompt(templates: None) -> langchain_core.prompts.prompt.PromptTempla
     return qa_chain_prompt
 
 
-def create_qa_chain(llm: any, database_vector: any, prompt: any) -> langchain.chains.retrieval_qa.base.RetrievalQA:
+def create_qa_chain(llm: any, prompt: any) -> langchain.chains.retrieval_qa.base.RetrievalQA:
+    _,hf_embedding_model, _, url_database, api_key_database = load_auguments()
+
+    client = qdrant_client.QdrantClient(
+            url = url_database,
+            api_key= api_key_database
+        )
+    
+    embeddings = HuggingFaceEmbeddings(model_name = hf_embedding_model)
+    doc_store = Qdrant(
+        client=client, collection_name="Pengi-Doc", 
+        embeddings=embeddings,
+    )
+
+
     memory = ConversationBufferMemory(
             memory_key='chat_history', return_messages=True)
     
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type='stuff',  # Change 'stuff' to a valid chain type
-        retriever=  database_vector.as_retriever(search_kwargs={"k": 3}),
+        retriever=  doc_store.as_retriever(search_kwargs={"k": 3}),
         return_source_documents = False, #trả về src trả lời
         # memory = memory,
         chain_type_kwargs={
@@ -71,3 +89,21 @@ def create_qa_chain(llm: any, database_vector: any, prompt: any) -> langchain.ch
         }
     )
     return qa_chain
+
+def load_FAQ(msg):
+    _,hf_embedding_model, _, url_database, api_key_database = load_auguments()
+    model_embedings = SentenceTransformer(hf_embedding_model)
+    query_vector = model_embedings.encode(msg).tolist()
+
+
+    client = qdrant_client.QdrantClient(
+            url = url_database,
+            api_key= api_key_database
+        )
+    hits = client.search(
+    collection_name = "Pengi-FAQ",
+    query_vector = query_vector,
+    limit = 1,
+    )
+
+    return hits
