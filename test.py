38161e41c840dc5a43e1f956bd23e15335404195
lilmaxie <hoangminh261003@gmail.com>
2024-04-07 21:05:37 +0700

    Phase2/HoangLM: Update file test.py

--- test.py
+++ test.py
@@ -12,59 +12,34 @@ from langchain_core.prompts import PromptTemplate
 from langchain_community.vectorstores import Qdrant
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain_community.llms import CTransformers
+
 def API_key() -> None:
     os.environ["QDRANT_HOST"] = "https://ef3d190d-4471-4044-b8a5-6aba8c656aa8.us-east4-0.gcp.cloud.qdrant.io:6333"
     os.environ["QDRANT_API_KEY"] = "EPOl1GIG9WTr2joExRzwjRkk5LuLWNvImfgU4y2GPaN_Nu5kWcs51w"
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
-
-
-def generate_prompte_template():
-    template = """<|im_start|>system\nBạn là một trợ lý AI hữu ích. Bạn chỉ sử dụng những thông tin mà bạn được cung cấp để trả lời các câu hỏi,
-    hãy trả lời câu hỏi một cách ngắn gọn, trung thực và chính xác. Tránh trả lời các câu hỏi không có trong thông tin mà bạn được cung cấp.
-    Nếu câu hỏi không liên quan đến nội dung mà bạn được cung cấp, bạn hãy trả lời rằng bạn không biết câu trả lời.Tuyệt đối không được bịa ra
-    câu trả lời.\n{context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
-
-    # template = """
-    # <|im_start|>system\nBạn là một trợ lý AI hữu ích. Bạn chỉ sử dụng những thông tin mà bạn được cung cấp để trả lời các câu hỏi,
-    # hãy trả lời câu hỏi một cách ngắn gọn, trung thực và chính xác. Tránh trả lời các câu hỏi không có trong thông tin mà bạn được cung cấp.
-    # Nếu câu hỏi không liên quan đến nội dung mà bạn được cung cấp, bạn hãy trả lời rằng "Tôi không biết câu trả lời".Tuyệt đối không được sử dụng những kiến thức
-    # mà bạn đã học để tạo ra câu trả lời\n{context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
-
-
-    # template = """
-    # <|im_start|>system\nBạn là một bậc thầy trong công việc làm trợ lý ảo. Bạn chỉ được sử dụng những thông tin mà bạn được cung cấp để trả lời các câu hỏi,
-    # hãy trả lời câu hỏi một cách ngắn gọn, trung thực và chính xác. Tránh trả lời các câu hỏi không có trong thông tin mà bạn được cung cấp.
-    # Nếu câu hỏi không liên quan đến nội dung mà bạn được cung cấp, bạn hãy trả lời rằng "Tôi không biết câu trả lời". Bỏ qua những tìm kiếm ngoại lệ không liên quan đến câu hỏi.
-    # Không được sử dụng những kiến thức mà bạn đã học để bịa ra câu trả lời\n{context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
-    
-    return template
-
-
-def chatbot(model_path) -> dict:
+def create_embeddings():
     client = qdrant_client.QdrantClient(
         url=os.getenv("QDRANT_HOST"),
         api_key=os.getenv("QDRANT_API_KEY")
     )
-
     embeddings = HuggingFaceEmbeddings(model_name='keepitreal/vietnamese-sbert')
-    doc_store = Qdrant(
-        client=client, collection_name="Pengi-Doc",
-        embeddings=embeddings,
-    )
-
-    config = {'context_length' : 2048}
-    llm = CTransformers(model = model_path,model_type ="llama", max_new_token=1024,temperature = 0.01,config= config)
-
+    return embeddings, client
  
-#     template = """<|im_start|>system\nChỉ sử dụng thông tin sau đây để trả lời câu hỏi. Nếu câu hỏi không liên quan dến nội dung sau đây, hãy trả lời rằng bạn không biết câu trả lời, đừng cố sinh thêm thông tin để trả lời\n
-# {context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
-    template = generate_prompte_template()
-    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+def create_qa_chain(model_path, doc_store):
+    config = {'context_length' : 2048}
+    llm = CTransformers(model=model_path, model_type="llama", max_new_token=1024, temperature=0.01, config=config)
+
+    template = """system\nBạn là một trợ lý AI hữu ích. Bạn chỉ sử dụng những thông tin mà bạn được cung cấp để trả lời các câu hỏi,
+    hãy trả lời câu hỏi một cách ngắn gọn, trung thực và chính xác. Tránh trả lời các câu hỏi không có trong thông tin mà bạn được cung cấp.
+    Nếu câu hỏi không liên quan đến nội dung mà bạn được cung cấp, bạn hãy trả lời rằng bạn không biết câu trả lời.Tuyệt đối không được bịa ra
+    câu trả lời.\n{context}\nuser\n{question}\nassitant"""
+
+    QA_CHAIN_PROMPT = PromptTemplate.from_template(input_variables=["context", "question"], template=template)
 
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
-        chain_type='stuff',  
+        chain_type='stuff',
         retriever=doc_store.as_retriever(search_kwargs={"k": 3}),
         verbose=True,
         chain_type_kwargs={
@@ -80,11 +55,9 @@ def run_and_save(qa_chain: dict, input_file_path: str, save_csv: str, wait_time:
     yêu cầu bộ data phải chuẩn format, cuối mỗi câu hỏi phải có dấu chấm hỏi
     không khoảng trắng so với kí tự cuối, mỗi câu không cần có enter xuống dòng
     """
-    # if torch.cuda.is_available():
-    #     device = torch.cuda.get_device_name()
     
     with open(save_csv, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['query', 'result', 'inference_time', 'device']
+        fieldnames = ['query', 'result', 'inference_time']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
@@ -100,9 +73,7 @@ def run_and_save(qa_chain: dict, input_file_path: str, save_csv: str, wait_time:
 
                     result_text = result['result'] if 'result' in result else ''
 
-
                     writer.writerow({'query': query, 'result': result_text, 'inference_time': inference_time})
-
                     #time.sleep(wait_time)
 
 def main() -> None:
@@ -110,8 +81,10 @@ def main() -> None:
     data_path = "data\Test_FAQ.txt"
     result_path = "data\KhanhDB_result_on_gguf.csv"
     API_key()
-    qa_chain = chatbot(model_path)
-    run_and_save(qa_chain,data_path, result_path)
+    embeddings, client = create_embeddings()
+    doc_store = Qdrant(client=client, collection_name="Pengi-Doc", embeddings=embeddings)
+    qa_chain = create_qa_chain(model_path, doc_store)
+    run_and_save(qa_chain, data_path, result_path)
 
 if __name__ == "__main__":
     main()
