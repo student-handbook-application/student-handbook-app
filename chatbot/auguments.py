c82555b7ac325d46cd20d9def403ac167fcc5223
lilmaxie <hoangminh261003@gmail.com>
2024-04-08 12:07:35 +0700

    Phase2/KhanhDB: Update code

--- chatbot/auguments.py
+++ chatbot/auguments.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 
 def load_auguments():
     load_dotenv(".env")
+    hf_api = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
     hf_embedding_model = os.environ.get("hf_embedding_model")
+    model_id = os.environ.get("model_id")
     url_database = os.environ.get("QDRANT_HOST")
     api_key_database = os.environ.get("QDRANT_API_KEY")
-    return hf_embedding_model, url_database, api_key_database
+    return hf_api ,hf_embedding_model, model_id, url_database, api_key_database
