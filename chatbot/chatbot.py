39347c112affaca400e6a420ec86bdd24df58731
Khanhduong123 <khanhbaoduong789@gmail.com>
2024-04-08 12:13:06 +0700

    test

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,10 +1,7 @@
 from chatbot.model import *
 
 def Chatbot(msg,llm):
-    template = """<|im_start|>system\nBạn là một trợ lý AI hữu ích. Bạn chỉ sử dụng những thông tin mà bạn được cung cấp để trả lời các câu hỏi,
-    hãy trả lời câu hỏi một cách ngắn gọn, trung thực và chính xác. Tránh trả lời các câu hỏi không có trong thông tin mà bạn được cung cấp.
-    Nếu câu hỏi không liên quan đến nội dung mà bạn được cung cấp, bạn hãy trả lời rằng bạn không biết câu trả lời.Tuyệt đối không được bịa ra
-    câu trả lời.\n{context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
+    
 
     #load FAQ database
     hits = load_FAQ(msg)
@@ -14,6 +11,10 @@ def Chatbot(msg,llm):
         if hit.score > 0.49:
             return f"{hit.payload['Answers']}"
         else:
+            template = """<|im_start|>system\nBạn là một trợ lý AI hữu ích. Bạn chỉ sử dụng những thông tin mà bạn được cung cấp để trả lời các câu hỏi,
+    hãy trả lời câu hỏi một cách ngắn gọn, trung thực và chính xác. Tránh trả lời các câu hỏi không có trong thông tin mà bạn được cung cấp.
+    Nếu câu hỏi không liên quan đến nội dung mà bạn được cung cấp, bạn hãy trả lời rằng bạn không biết câu trả lời.Tuyệt đối không được bịa ra
+    câu trả lời.\n{context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assitant"""
             prompt = create_prompt(template)
             qa_chain = create_qa_chain(llm, prompt)
             return qa_chain.invoke({"query": msg})
