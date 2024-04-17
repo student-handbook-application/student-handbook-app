d3d474ae1bf6869b53aab8cfd998dd978d316029
Khanhduong123 <khanhbaoduong789@gmail.com>
2024-04-17 14:47:13 +0700

    Phase2/KhanhDB: fix bug code

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,23 +1,17 @@
 from chatbot.model import *
 def Chatbot(msg,llm):
-    template = """<|im_start|>system
-Bạn là một trợ lý AI hữu ích. Bạn chỉ sử dụng những thông tin mà bạn được cung cấp và lịch sử cuộc trò chuyện để trả lời các câu hỏi.Hãy trả lời câu hỏi một cách ngắn gọn, trung thực và chính xác. Tránh trả lời các câu hỏi không có trong thông tin mà bạn được cung cấp.
-Nếu câu hỏi không liên quan đến nội dung mà bạn được cung cấp, bạn hãy trả lời rằng bạn không biết câu trả lời.Tuyệt đối không được bịa ra câu trả lời.\n
+    template = """You are an AI assistant and admissions consultant. Use the following pieces of context to answer the questions HONESTLY, ACCURATELY and MOST NATURAL. Avoid answering questions that are not included in the information you are provided. If the question is not related to the content you are provided, answer with SORRY I DON'T KNOW THE ANSWER. ABSOLUTELY not allowed. make up an answer.\n
 {context}
-<|im_end|>
-<|im_start|>user
-{question}<|im_end|>
-<|im_start|>assistant
-"""
-    
+Question: {question}
+Answer:"""
 
     hits = load_FAQ(msg)
 
     #run result
     for hit in hits:
-        if hit.score > 0.5:
+        if hit.score > 0.49:
             return f"{hit.payload['Answers']}"
         else: 
             prompt = create_prompt(template)
             qa_chain = create_qa_chain(llm, prompt)
-            return qa_chain.invoke(msg)
+            return qa_chain.invoke({"query": msg})
