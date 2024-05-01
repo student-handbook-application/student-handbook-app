d0f7a9362a114f0d7a650e587f91057521cd2678
lilmaxie <hoangminh261003@gmail.com>
2024-05-01 11:17:27 +0700

    Phase2/HoangLM: updated

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,15 +1,15 @@
 from chatbot.model import *
 def Chatbot(msg,llm):
-    template = """You are an AI assistant and admissions consultant. Use the following pieces of context to answer the questions HONESTLY, ACCURATELY and MOST NATURAL. Avoid answering questions that are not included in the information you are provided. If the question is not related to the content you are provided, answer with SORRY I DON'T KNOW THE ANSWER. ABSOLUTELY not allowed. make up an answer.\n
+    template = """You are an AI assistant and admissions consultant. Use the following pieces of context to answer the questions HONESTLY, ACCURATELY and MOST NATURAL. Avoid answering questions that are not included in the information you are provided. If the question is not related to the content you are provided, answer with TÔI KHÔNG BIẾT CÂU TRẢ LỜI. ABSOLUTELY not allowed. make up an answer.\n
 {context}
 Question: {question}
-Answer:"""
+Helpful Answer:"""
 
     hits = load_FAQ(msg)
 
     #run result
     for hit in hits:
-        if hit.score > 0.4:
+        if hit.score > 0.6:
             conversation_logs = [{"user_message": msg, "ai_response": hit.payload['Answers']}]
             save_conversation_history(conversation_logs)
             return f"{hit.payload['Answers']}"
